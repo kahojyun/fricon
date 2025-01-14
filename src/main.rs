4505be05d9c08fdde479f33b4bb044b2df6aeed5
Jiahao Yuan <kahojyun@icloud.com>
2025-01-14 19:41:00 +0800

    feat: basic dataset api (#3)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::Parser as _;
+use fricon::cli::Cli;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let cli = Cli::parse();
+    fricon::main(cli).await
 }
