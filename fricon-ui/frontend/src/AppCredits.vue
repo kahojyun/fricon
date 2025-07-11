bc802d8b24bbdaeeb65619f479c78690efb8b023
Jiahao Yuan <kahojyun@icloud.com>
2025-07-11 17:00:57 +0800

    Change icon

---
+++ fricon-ui/frontend/src/AppCredits.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { Button } from "primevue";
+</script>
+<template>
+  <Button
+    link
+    as="a"
+    href="https://www.flaticon.com/free-icons/cube"
+    title="cube icons"
+    target="_blank"
+    label="Cube icons created by Freepik - Flaticon"
+  />
+</template>
