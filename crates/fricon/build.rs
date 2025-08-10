435c9e437f2011f90841ba2441faa1f948ef7a4b
Jiahao Yuan <kahojyun@icloud.com>
2025-08-10 16:57:10 +0800

    Update Rust dependencies and MSRV
    Upgrade Arrow, PyO3, Prost, and Tonic related crates to their latest
    versions. This also involves migrating from `tonic-build` to
    `tonic-prost-build` for protobuf compilation. The minimum supported Rust
    version (MSRV) is increased to 1.89.

--- crates/fricon/build.rs
+++ crates/fricon/build.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, fs};
+use std::{error::Error, fs, path::PathBuf};
 
 fn main() -> Result<(), Box<dyn Error>> {
     println!("cargo::rerun-if-changed=migrations");
@@ -6,8 +6,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     for p in fs::read_dir("proto/fricon/v1")? {
         protos.push(p?.path());
     }
-    tonic_build::configure()
-        .bytes(["."])
-        .compile_protos(&protos, &["proto"])?;
+    tonic_prost_build::configure()
+        .bytes(".")
+        .compile_protos(&protos, &[PathBuf::from("proto")])?;
     Ok(())
 }
